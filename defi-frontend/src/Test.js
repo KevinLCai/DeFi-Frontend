6d782e2908c19fccec19996eb5ac67ebd89bbbfe
Kevin Cai <kevincai@Kevins-MBP.home>
2023-03-26 22:11:58 +0100

    frontend table

--- defi-frontend/src/Test.js
+++ defi-frontend/src/Test.js
@@ -4,14 +4,16 @@ import socketIOClient from 'socket.io-client';
 const ENDPOINT = "http://localhost:3000"; // the endpoint where the websocket is running
 
 function App() {
-  const [dataFromBackend, setDataFromBackend] = useState("");
+  const [dataFromBackend, setDataFromBackend] = useState([]);
+  const tableHeaders = ["Strategy", "Token ID", "Timestamp", "Order Type", "Price", "Size"];
 
   useEffect(() => {
     const socket = socketIOClient(ENDPOINT);
 
     socket.on("data_from_backend", (data) => {
       console.log(`Received data from backend: ${JSON.stringify(data)}`);
-      setDataFromBackend(data.data.message);
+      const { strategy, tokenID, timestamp, orderType, price, size } = data.data;
+      setDataFromBackend(prevData => [...prevData, { strategy, tokenID, timestamp, orderType, price, size }]);
     });
 
     return () => {
@@ -21,7 +23,28 @@ function App() {
 
   return (
     <div>
-      <p>Data from backend: {dataFromBackend}</p>
+      <h2>Data from backend:</h2>
+      <table>
+        <thead>
+          <tr>
+            {tableHeaders.map(header => (
+              <th key={header}>{header}</th>
+            ))}
+          </tr>
+        </thead>
+        <tbody>
+          {dataFromBackend.map((data, index) => (
+            <tr key={index}>
+              <td>{data.strategy}</td>
+              <td>{data.tokenID}</td>
+              <td>{data.timestamp}</td>
+              <td>{data.orderType}</td>
+              <td>{data.price}</td>
+              <td>{data.size}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </div>
   );
 }
