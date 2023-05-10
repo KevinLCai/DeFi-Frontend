44b330d4c68d94c8feb3f88b791a2ba4b27c90e9
Kevin Cai <kevinlcai@outlook.com>
2023-05-10 10:27:54 +0100

    partially working

--- defi-frontend/src/TradingDataDisplay.js
+++ defi-frontend/src/TradingDataDisplay.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import io from 'socket.io-client';
 
-const socket = io('http://localhost:3001/cefi_deal');
+const socket = io('http://localhost:3000/cefi_deal');
 
 function App() {
   const [tradeData, setTradeData] = useState([]);
